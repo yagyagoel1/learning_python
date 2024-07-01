a6bc066d63bd81d77e2b87d481bbdd22f5f5056b
yagyagoel1 <yagyagoel87@gmail.com>
2024-07-01 22:28:54 +0530

    added the solution 1

---
+++ 03_loops/01-solutions.py
@@ -0,0 +1,9 @@
+number  = [1,-2,3,-4,5,6,-7,-8,9,10]
+
+positive_no_count=0
+for num in numbers:
+    if num>0:
+        positive_no_count+=1
+
+
+print("The count of positive numbers are: ", positive_no_count)
