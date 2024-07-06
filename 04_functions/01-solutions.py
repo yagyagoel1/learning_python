f9a04a3fba915d4da1477904782808bb88dd71ec
yagyagoel1 <yagyagoel87@gmail.com>
2024-07-06 14:15:53 +0530

    wrote the solution of 1 and 2

---
+++ 04_functions/01-solutions.py
@@ -0,0 +1,5 @@
+def square_of_num():
+    num = int(input("Enter a number: "))
+    return num**2
+
+print(square_of_num())
---
+++ 04_functions/02-solutions.py
@@ -0,0 +1,7 @@
+def sum_of_num(num1,num2):
+    return num1 + num2
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+print(sum_of_num(num1,num2))
+
